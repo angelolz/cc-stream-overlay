2f5a1883da6e9b370ca09ddea153e59fb868e96f
Angel Legaspi <angel.lugia98@gmail.com>
2023-06-11 15:04:39 -0600

    changed timeout from 5 seconds to 15 seconds

--- overlay.js
+++ overlay.js
@@ -1,7 +1,8 @@
 /* global vars */
-var cycleNum = 0;
-var timer;
-var cookieData = null;
+let cycleNum = -1;
+let cookieData = null;
+let animTimeMs = 500;
+let cycleTime = 15000;
 
 $(function() {
     setupWebsocket();
@@ -12,58 +13,79 @@ function setupWebsocket()
     const ws = new WebSocket("ws://localhost:6969/ws");
 
     //websocket events
-    ws.onopen = function(event) {
-        timer = setInterval(() => {
-            updateStats();
-
-            if(cycleNum === 5) {
-                cycleNum = 0;
-                console.log("cycleNum: ", cycleNum)
-                return;
-            }
-
-            cycleNum++;
-            console.log("cycleNum: ", cycleNum)
-
-        }, 15000)
-    }
-
-    ws.onclose = function(event) {
-        clearInterval(timer);
-        console.log("cleared timer")
+    ws.onopen = function() {
+        console.log("connected");
     }
 
     ws.onmessage = function(event) {
+        $("#data").html(event.data);
         console.log(event.data);
         cookieData = JSON.parse(event.data);
-        updateStats();
-        $("#data").html(event.data);
+        if(cycleNum === -1)
+        {
+            changeCycle();
+            updateStats();
+        }
     };
 }
 
 function updateStats() {
-    const statsElement = $("#stats");
+    triggerTransitions();
+    setTimeout(updateText, animTimeMs);
+    changeCycle();
+    setTimeout(updateStats, cycleTime);
+}
 
-    if(cookieData == null) return;
+function updateText() {
+    const statNameElement = $("#stat-name");
+    const statValueElement = $("#stat-value");
 
     switch(cycleNum) {
         case 0: //bank
-            statsElement.html(cookieData.cookies);
+            statNameElement.html("Cookie Bank");
+            statValueElement.html(cookieData.cookies);
             break;
         case 1: //cps
-            statsElement.html(`${cookieData.cps}/sec`);
+            statNameElement.html("Cookies per second");
+            statValueElement.html(`${cookieData.cps}/sec`);
             break;
         case 2: //prestige lv
-            statsElement.html(`Prestige Lv:  ${cookieData.prestige_lvl}`);
+            statNameElement.html("Prestige Level");
+            statValueElement.html(cookieData.prestige_lvl);
             break;
         case 3: //sugar lumps
-            statsElement.html(`${cookieData.lumps} sugar lumps`);
+            statNameElement.html("Sugar lumps");
+            statValueElement.html(`${cookieData.lumps} sugar lumps`);
             break;
         case 4: //clicks
-            statsElement.html(`${cookieData.clicks} clicks`);
+            statNameElement.html("Clicks");
+            statValueElement.html(cookieData.clicks);
             break;
         case 5: //clicks
-            statsElement.html(`${cookieData.gc_clicks} golden cookies`);
+            statNameElement.html("Golden Cookies Clicked");
+            statValueElement.html(cookieData.gc_clicks);
             break;
+    }
+}
+
+function triggerTransitions() {
+    $("#stat-name").addClass("transition");
+    $("#stat-value").addClass("transition");
+    setTimeout(() => clearTransitions("#stat-name"), (animTimeMs * 2));
+    setTimeout(() => clearTransitions("#stat-value"), (animTimeMs * 2));
+}
+
+function clearTransitions(id) {
+    $(id).removeClass("transition");
+}
+
+function changeCycle()
+{
+    if(cycleNum === 5) {
+        cycleNum = 0;
+    }
+
+    else {
+        cycleNum++;
     }
 }
